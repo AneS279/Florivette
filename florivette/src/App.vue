77badcef9dd46b65bfb432ce61086b19ece2aeb0
AneS279 <anes.2017@alunos.utfpr.edu.br>
2022-11-15 01:49:01 -0300

    Criando Footer

--- florivette/src/App.vue
+++ florivette/src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import Background_Image from './views/First_Page.vue'
 import Menu from './views/Menu.vue';
+import footerVue from './views/footer.vue';
 </script>
     <template>
       <router-view></router-view>
@@ -13,6 +14,7 @@ import Menu from './views/Menu.vue';
       <br>
       <router-link to="/">Trabalhe Conosco</router-link>
     </Menu>
+    <footerVue></footerVue>
 </template>
 
   <style scoped>
---
+++ florivette/src/views/footer.vue
@@ -0,0 +1,17 @@
+<template>
+    <footer>
+      <a href="facebook.com"></a>
+    </footer>
+</template>
+<style>
+footer{
+    margin-top: 5px;
+    bottom: -91vh;
+    background: rgb(83,15,39);
+    background: linear-gradient(90deg, rgba(83,15,39,1) 0%, rgba(50,18,57,1) 50%, rgba(5,32,91,1) 100%);
+    width: 100%;
+    left: 0px;
+    height: 55%;
+    position: absolute;
+}
+</style>
